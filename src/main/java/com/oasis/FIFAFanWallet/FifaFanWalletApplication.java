5fe2cdb57166c64a033f67de0fcd9a7cb37a5e35
Natanim Issa <natanimissa@gmail.com>
2026-06-03 09:43:54 -0400

    Fifa Fan Wallet project initialized

---
+++ src/main/java/com/oasis/FIFAFanWallet/FifaFanWalletApplication.java
@@ -0,0 +1,13 @@
+package com.oasis.FIFAFanWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaFanWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaFanWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oasis/FIFAFanWallet/FifaFanWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.FIFAFanWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaFanWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
